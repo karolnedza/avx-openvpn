4253c67a8a7d9df4125daf9cb9c854a881e2ebaf
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-12-07 17:08:52 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,9 @@
 variable "vcs_repository" {}
-
-
-variable "domain_name_1" {}
-
-
-variable "domain_name_2" {}
-
-
+variable "vpc_id_vpn" {default = "vpc-082267b29868e6e92"}
+variable "gw_name_vpn" {default = "Aviatrix-vpc-082267b20x129c8d52"}
+variable "profiles" {}
+variable "user_name_vpn" {}
 variable "ctrl_password" {}
+
+
+
