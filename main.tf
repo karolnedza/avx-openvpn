25740c16bd254959b36c092b1578eccc829e3b16
karolnedza <37583943+karolnedza@users.noreply.github.com>
2020-12-07 15:51:56 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aviatrix_vpn_user" "vpn_user" {
-   vpc_id = var.gw_vpn_vpc
-   gw_name    = var.gw_vpn_name
+   vpc_id = var.vpc_id_vpn
+   gw_name    = var.gw_name_vpn
    user_name  = var.user_name_vpn
    user_email = var.user_email_vpn
 }
