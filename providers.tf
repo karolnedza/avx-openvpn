38b1b49a23a1c542a99aa16a37b096fc560a756a
karolnedza <37583943+karolnedza@users.noreply.github.com>
2021-03-06 16:06:56 +0100

    Update providers.tf

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 provider "aviatrix" {
   username     = "admin"
-  password      = var.ctrl_ip
-  controller_ip = "18.156.141.82"
-  version       = "2.17.0"
+  password      = var.ctrl_password
+  controller_ip = var.ctrl_ip
+  version       = "2.18.0"
 }
